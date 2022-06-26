a1d779ed3c2262d98fff8de7013e38255931ab2f
Bonie Ferrer <bonieferrer08@yahoo.com>
2022-06-26 11:47:30 -0400

    Hello World in NetBeans

---
+++ classwork/myJavaBootCamp2.0/HelloWorld/src/main/java/com/mthreebonf/helloworld/Hello.java
@@ -0,0 +1,14 @@
+/*          :Bonie Ferrer
+ * email    :bonieferrer08@yahoo.com
+ *          :June26,2022
+ *          :My first Hello World program in an IDE
+ */
+package com.mthreebonf.helloworld;
+
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Bonie!");
+    }
+    
+}
